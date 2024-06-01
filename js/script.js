1bdfd9fdb41d5c2269fda0bfacc23f631576659d
Habibur Rahman <habib.ahmedofficial02@gmail.com>
2024-06-01 10:02:23 +0600

    Navbar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+
+document.addEventListener("mousemove",function(e){
+  cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+})
